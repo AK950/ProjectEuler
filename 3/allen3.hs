25b62776c52b8cb61041d693eb17bc6962dbf7b4
Allen Kim <akim003@citymail.cuny.edu>
2014-11-09 08:21:25 -0500

    Solved problem 3

--- 3/allen3.hs
+++ 3/allen3.hs
@@ -1,2 +1,10 @@
 --Project Euler
 --Problem 3
+
+smallFac x = head [num | num<-[2..x], x `mod` num == 0]
+
+largestPrime 1 = []
+largestPrime x = fac : largestPrime (x `div` fac)
+						where fac = smallFac x
+						
+answer = maximum $ largestPrime 600851475143
