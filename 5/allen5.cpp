c136470a85f5003b37c2c1b5040c345dcc7e8ad1
Allen Kim <akim003@citymail.cuny.edu>
2014-11-23 21:46:12 -0500

    Solved problem 5, Allen

---
+++ 5/allen5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int gcf(int, int);
+
+int main(){
+	int answer = 1;
+	for (int i = 20; i > 0; --i)
+	{
+		answer *= i/gcf(answer,i);
+	}
+	std::cout << answer << std::endl;
+	return 0;
+}
+
+int gcf(int a, int b){
+	if (b==0)
+		return a;
+	return gcf(b,a % b);
+}
